2aa2030cae5abe5f12d25f7ca29c79dc85bb9cf4
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2025-10-21 12:27:42 -0700

    u

--- contacts.js
+++ contacts.js
@@ -4,27 +4,29 @@ function sleep(ms) {
     });
 }
 
-window.exportedContactsStorage = new Set();
-window.scroller = document.querySelector('.My2mLb');
-while (scroller.scrollHeight - scroller.scrollTop > scroller.clientHeight) {
-    for (let element of document.querySelectorAll('.ZvpjBb.C8Dkz')[0].querySelectorAll('.XXcuqd')) {
-        if (element.firstChild.childNodes.length == 1) {
-            break;
+window.queueExport = async function() {
+    window.exportedContactsStorage = new Set();
+    window.scroller = document.querySelector('.My2mLb');
+    while (scroller.scrollHeight - scroller.scrollTop > scroller.clientHeight) {
+        for (let element of document.querySelectorAll('.ZvpjBb.C8Dkz')[0].querySelectorAll('.XXcuqd')) {
+            if (element.firstChild.childNodes.length == 1) {
+                break;
+            }
+            let name = element.firstChild.childNodes[1].innerText;
+            let job = element.firstChild.childNodes[2].innerText;
+            let email = element.firstChild.childNodes[3].innerText;
+            let phone = element.firstChild.childNodes[4].innerText;
+            window.exportedContactsStorage.add(JSON.stringify({'name': name, 'job': job, 'email': email, 'phone': phone}));
         }
-        let name = element.firstChild.childNodes[1].innerText;
-        let job = element.firstChild.childNodes[2].innerText;
-        let email = element.firstChild.childNodes[3].innerText;
-        let phone = element.firstChild.childNodes[4].innerText;
-        window.exportedContactsStorage.add(JSON.stringify({'name': name, 'job': job, 'email': email, 'phone': phone}));
+        scroller.scrollTo({
+            top: scroller.scrollTop + scroller.clientHeight,
+            behavior: 'smooth'
+        });
+        console.log(
+            'Completed iteration;',
+            scroller.scrollTop.toString() + '/' + scroller.scrollHeight.toString() +
+            ' = ' + (scroller.scrollTop / scroller.scrollHeight * 100).toString() + '%'
+        );
+        await sleep(4000);
     }
-    scroller.scrollTo({
-        top: scroller.scrollTop + scroller.clientHeight,
-        behavior: 'smooth'
-    });
-    console.log(
-        'Completed iteration;',
-        scroller.scrollTop.toString() + '/' + scroller.scrollHeight.toString() +
-        ' = ' + (scroller.scrollTop / scroller.scrollHeight * 100).toString() + '%'
-    );
-    await sleep(4000);
 }
--- popup.js
+++ popup.js
@@ -1,1 +1,14 @@
 // console.log('This is a popup!');
+document.getElementById('exportBtn').addEventListener('click', () => {
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        chrome.scripting.executeScript({
+            target: {tabId: tabs[0].id},
+            files: ['contacts.js']
+        }, () => {
+            chrome.scripting.executeScript({
+                target: {tabId: tabs[0].id},
+                function: window.queueExport
+            });
+        });
+    });
+});
